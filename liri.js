0442537c96fd54a0f484dcbc04251551760a38e8
ThePalad1n <evangreen91@gmail.com>
2020-04-20 13:35:49 -0400

    liri changes

---
+++ liri.js
@@ -0,0 +1,47 @@
+
+
+const dotenv = require('dotenv').config()
+var moment = require('moment');
+moment().format()
+
+const axios = require('axios');
+ 
+// Make a request for a user with a given ID
+axios.get('/user?ID=ThePalad1n')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+ 
+// Optionally the request above could also be done as
+axios.get('/user', {
+    params: {
+      ID: ThePalad1n
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });  
+ 
+// Want to use async/await? Add the `async` keyword to your outer function/method.
+async function getUser() {
+  try {
+    const response = await axios.get('/user?ID=ThePalad1n');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
