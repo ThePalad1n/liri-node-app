70a854fa04835e18a8ada65e66e52d897079200e
ThePalad1n <evangreen91@gmail.com>
2020-04-20 16:23:06 -0400

    concert function spotify not

--- liri.js
+++ liri.js
@@ -1,68 +1,11 @@
 const dotenv = require('dotenv').config();
 var moment = require('moment');
 moment().format();
-var request = require('request')
 var keys = require("./keys.js");
 
 const axios = require('axios');
-/* Axios example 
-// Make a request for a user with a given ID
-axios.get('/user?ID=ThePalad1n')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
- 
-// Optionally the request above could also be done as
-axios.get('/user', {
-    params: {
-      ID: ThePalad1n
-    }
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });  
- 
-// Want to use async/await? Add the `async` keyword to your outer function/method.
-async function getUser() {
-  try {
-    const response = await axios.get('/user?ID=ThePalad1n');
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-}
-*/
 
-//spotify example
 
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: '1d59844cc57840288131ecdf8d4bb81f',
-  secret: 'ee6e163fb617422ca234cad359914bde'
-});
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
 
 
 //vars for user input
@@ -76,6 +19,86 @@ for (var i = 3; i < cmdArgs.length; i++) {
   liriArg += cmdArgs[i] + '';
 }
 
+
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+  id: '1d59844cc57840288131ecdf8d4bb81f',
+  secret: 'ee6e163fb617422ca234cad359914bde'
+});
+
+function spotifySong(song) {
+
+  var search;
+  if (song === '') {
+    search = 'The Sign Of Ace';
+  } else {
+    search = song;
+  }
+
+  spotify.search({ type: 'track', query: search }, function (error, data) {
+    if (error) {
+      var error1 = 'ERROR: Retrieving Spotify track -- ' + error;
+      console.log(error1);
+      return;
+    }
+    else {
+      var songInfo = data.tracks.items[0];
+      if (!songinfo) {
+        var error2 = 'ERROR: No song info retrieved';
+        console.log(error2);
+        return;
+      }
+      else {
+        var outputStr = '--------------\n' +
+          'Song Information:\n' +
+          '---------------------\n\n' +
+          'Song Name: ' + songInfo.name + '\n' +
+          'Artist: ' + songInfo.artist[0].name + '\n' +
+          'Album: ' + songInfo.album.name + '\n' +
+          'Preview Here: ' + songInfo.preview_url + '\n';
+        console.log(outputStr)
+      }
+    }
+  });
+}
+
+/*
+function spotifySong(song) {
+  var search;
+  if (song === '') {
+    search = 'ADHD';
+  } else {
+    search = song;
+  }
+
+  spotify.search({ type: 'track', query: search }, function (error, data) {
+    if (error) {
+      var error1 = 'ERROR: Retrieving Spotify track -- ' + error;
+      console.log(error1);
+      return;
+    }
+    else {
+      var songInfo = data.tracks.items[0];
+      if (!songinfo) {
+        var errorStr2 = 'ERROR: No song info retrieved';
+        console.log(errorStr2);
+        return;
+      }
+      else {
+        var outputSong = '--------------\n' +
+          'Song Information:\n' +
+          '---------------------\n\n' +
+          'Song Name: ' + songInfo.name + '\n' +
+          'Artist: ' + songInfo.artist[0].name + '\n' +
+          'Album: ' + songInfo.album.name + '\n' +
+          'Preview Here: ' + songInfo.preview_url + '\n';
+        console.log(outputSong)
+      }
+    }
+  });
+}
+*/
 
 
 
@@ -92,39 +115,31 @@ function retEventInfo(artist) {
 
   var eventSearch = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-  request(eventSearch, function (error, response, body) {
-
-    if (error || (response.statusCode !== 200)) {
-      var error1 = 'ERROR: Retrieving bands_in_town entry -- ' + error;
-      console.log(error1)
-      return;
-
-    }
-
-    else {
-      var data = JSON.parse(body);
-
-      if (!data.venue && !data.location && !data.datetime) {
-        var error2 = 'ERROR: No event info found';
-        console.log(error2)
-        return;
-      } 
-
-      else {
-
+  axios.get(eventSearch)
+    .then(function (response) {
+      for (var i = 0; i < response.data.length; i++) {
         var outputEvent = '------------------------\n' +
           'Event Information:\n' +
           '------------------------\n\n' +
-          'Name of Venue: ' + data.venue + '\n' +
-          'Location: ' + data.location + '\n' +
-          'Date & Time: ' + data.datetime + '\n' +
+          'Name of Venue: ' + response.data[i].venue.name + '\n' +
+          'Location: ' + response.data[i].venue.location + '\n' +
+          'Date & Time: ' + response.data[i].datetime + '\n' +
           '------------------------' + '\n'
         console.log(outputEvent);
       }
-    }
-  });
-
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
 }
+
+
+
+
 
 
 
