ace5afe7ac8a62fd6146085ed1528cd239eecf99
ThePalad1n <evangreen91@gmail.com>
2020-04-20 14:02:28 -0400

    what each cmd should do step 1

--- liri.js
+++ liri.js
@@ -5,7 +5,7 @@ moment().format();
 var keys = require("./keys.js");
 
 const axios = require('axios');
- 
+/* Axios example 
 // Make a request for a user with a given ID
 axios.get('/user?ID=ThePalad1n')
   .then(function (response) {
@@ -45,9 +45,38 @@ async function getUser() {
     console.error(error);
   }
 }
+*/
+
+//spotify example
+//var spotify = new Spotify(keys.spotify);
 
 
 
+//vars for user input
+var cmdArgs = process.argv;
+var liriCmd = process.argv[2];
+var liriArg = '';
 
+//determining th Arg
+for(var i=3; i<cmdArgs.length; i++){
+	liriArg += cmdArgs[i] + '';
+}
 
-var spotify = new Spotify(keys.spotify);
+// Determine which LIRI command is being requested
+
+//tweets
+if (liriCmd === 'my-tweets') {
+	retrieveTweets(); 
+
+  //spotify songs
+} else if (liriCmd === `spotify-this-song`) {
+	spotifySong(liriArg);
+
+  //movies  
+} else if (liriCmd === `movie-this`) {
+	retrieveOBDBInfo(liriArg);
+
+  //simeon says
+} else if (liriCmd ===  `do-what-it-says`) {
+	doAsTheySay();
+}
